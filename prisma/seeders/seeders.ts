2c8737d5e75f74eeb6995ccd1d93246447b29421
bayuubay <bayu.suryo.a@gmail.com>
2023-04-03 12:38:41 +0700

    update seeders

--- prisma/seeders/seeders.ts
+++ prisma/seeders/seeders.ts
@@ -3,8 +3,11 @@ import {prisma} from '../../src/database/client'
 // insert status
 import {Status} from './status.json'
 const seed_status =async () => {
+  const exist = (await prisma.type.findMany()).map(d => d.name)
   for (const s of Status) {
-    await prisma.status.create({data: s})
+    if(!exist.includes(s.name)){
+      await prisma.status.create({data: s})
+    }
   }
 }
 
@@ -12,30 +15,50 @@ const seed_status =async () => {
 
 import {Type} from './type.json'
 const seed_type =async () => {
+  const exist = (await prisma.type.findMany()).map(d => d.name)
   for (const s of Type) {
-    await prisma.type.create({data: s})
+    if(!exist.includes(s.name)){
+      await prisma.type.create({data: s})
+    }
   }
-  
 }
 
 //insert product
 import {Product} from './product.json'
 const seed_product =async () => {
-  
+  const exist = (await prisma.product.findMany()).map(d => d.sku)
   for (const s of Product) {
-    await prisma.product.create({data: s})
+    if(!exist.includes(s.sku)){
+      await prisma.product.create({data: s})
+    }
   }
 }
-
 
 //insert promotion
 
 import { Promotion } from './promotions.json'
 const seed_promotion = async () => {
-  const products = await prisma.product.findMany()
   const type = await prisma.type.findMany()
   for (const s of Promotion) {
-    await prisma.promotion.create({data: s})
+    let data = {
+      item_sku: s.item_sku,
+      description: s.description,
+      type_id: s.type_id,
+      percentage: s.percentage,
+      quantity: s.quantity,
+      min_qty: s.min_qty,
+      price: s.price,
+      bonus_item_sku: s.bonus_item_sku,
+    };
+    let type_id = type.find(({name})=> name === s.type)?.id
+    if(type_id) data.type_id = type_id
+    try {
+      if(type.length){
+        await prisma.promotion.create({data})
+      }
+    } catch (error) {
+      throw error
+    }
   }
 }
 seed_type(); seed_status(); seed_product();
